05be4714106cc2049c8d6e2dd898f48a5ae44867
Valery Ratsko <zitrasa@yandex.ru>
2025-09-23 22:29:28 +0300

    feat(torch): ensure that n-nlinear models are correct using mock dataset

--- src/baikal/ml/torch/dataset/__init__.py
+++ src/baikal/ml/torch/dataset/__init__.py
@@ -3,6 +3,9 @@ from baikal.ml.torch.dataset.arrow_time_series_dataset import (
     ArrowTimeSeriesDataset,
     ArrowTimeSeriesSample,
 )
+from baikal.ml.torch.dataset.mock_arrow_time_series_dataset import (
+    MockArrowTimeSeriesDataset,
+)
 from baikal.ml.torch.dataset.stride_window_strategy import StrideWindowStrategy
 from baikal.ml.torch.dataset.window_strategy import WindowStrategy
 
@@ -10,6 +13,7 @@ __all__ = [
     "ArrowTimeSeriesBatch",
     "ArrowTimeSeriesDataset",
     "ArrowTimeSeriesSample",
+    "MockArrowTimeSeriesDataset",
     "StrideWindowStrategy",
     "WindowStrategy",
 ]
